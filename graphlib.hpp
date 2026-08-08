1fc07263892c005721c70ab707b8cbac41e285c4
RANDOMFNP <randomfnp@outlook.com>
2026-08-08 17:22:29 -0400

    Add files via upload
    added graphlib.hpp

---
+++ graphlib.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void create_graph(unordered_map<int, vector<int>> graph, string output_file);
+void add_nodes(unordered_map<int, vector<int>> graph, string input_file);
